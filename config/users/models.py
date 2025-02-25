9c2576b73d1e92a040e9c874ab60e3dfeb27cc07
Amirreza Akbari <amrakbaridev@gmail.com>
2025-02-25 19:22:02 +0330

    created costum BaseUserManager

--- config/users/models.py
+++ config/users/models.py
@@ -1,9 +1,26 @@
 from django.contrib.auth.base_user import AbstractBaseUser
+from django.contrib.auth.models import BaseUserManager  as BUM
 from django.contrib.auth.models import PermissionsMixin
 from django.db import models
 
 from common.models import BaseModel
 
+
+class BaseUserManager(BUM):
+    def create_user(self, email, is_active=True, is_admin=False, password=None):
+        if not email:
+            raise ValueError('user must have an email address')
+
+        user = self.model(email=self.normalize_email(email), is_active=is_active, is_admin=is_admin)
+        if password:
+            user.set_password(password)
+        else:
+            user.set_unusable_password()
+
+        user.full_clean()
+        user.save()
+
+        return user
 
 class BaseUser(BaseModel, AbstractBaseUser, PermissionsMixin):
     email = models.EmailField(unique=True)
